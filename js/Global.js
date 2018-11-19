a7e44bcca72db6e992e124789586a3e90207261b
SimaoNunes <simaspedro1@gmail.com>
2018-11-19 00:45:21 +0000

    spaces

--- js/Global.js
+++ js/Global.js
@@ -2,16 +2,27 @@
 //  GLOBAL VARIABLES
 // ------------------
 var scene, mainScene, pauseScene, renderer, clock, delta; // variaveis gerais relativas a animacao
+
 var cameras, camera, camera1, camera2, controls;  // diferentes tipos de cameras
+
 var geometry, material, mesh;
+
 var board, ball, cube, wall; // diferentes objetos a desenhar
+
 var lights, currentLight, directionalLight, pointLight;  // diferentes tipos de luz
+
 var axisY = new THREE.Vector3(0,1,0);
+
 var counter;
+
 var lKey = false;   // variaveis que serao ativadas quando pressionadas as teclas respetivas
+
 var wKey = 0;
+
 var dKey = false;
+
 var pKey = false;
+
 var sKey = false;
 
 var lightCalculation = 1;
